3c775a5b455b54a104df6b8b36482b2bd67c172a
wizardcloth <ujjwal.chauhan8130@gmail.com>
2025-09-08 15:43:21 +0530

    completed

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://e-commerce-three-tau-34.vercel.app/login",
-    // origin: "*",
+    // origin: "https://e-commerce-three-tau-34.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
