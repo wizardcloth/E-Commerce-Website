ec62bf6164afacd46eb67e1cc22598cd7d8e5499
wizardcloth <ujjwal.chauhan8130@gmail.com>
2025-09-08 15:26:37 +0530

    bug fix

--- server/server.js
+++ server/server.js
@@ -7,8 +7,12 @@ import productRoutes from "./routes/productRoute.js";
 import cartRoutes from "./routes/cartRoute.js";
 
 dotenv.config();
+connectDB();
 
+const app = express();
+app.use(cors());
 app.use(express.json());
+
 app.use(
   cors({
     origin: "https://e-commerce-three-tau-34.vercel.app/login",
@@ -20,10 +24,6 @@ app.use(
   })
 );
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
@@ -33,7 +33,3 @@ app.get("/", (req, res) => res.send("Serverless Express API"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// export default async function handler(req, res) {
-//   await connectDB();
-//   app(req, res);
-// }
