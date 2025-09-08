9cf8c7c9df22c385d4614c8e8be03c08ffb63cfe
wizardcloth <ujjwal.chauhan8130@gmail.com>
2025-09-08 15:30:07 +0530

    completed

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+//production
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://e-commerce-website-liard-eta.vercel.app/",
 });
+
+//deployment
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3000/api",
+// });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
